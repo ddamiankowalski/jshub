aa63785ffd12fce73bf72a5a77a5fc0a2742a103
Damian Kowalski <dkowalski.1997@gmail.com>
2024-01-15 00:33:34 +0100

    added ssr controller

--- routes/route-controller.js
+++ routes/route-controller.js
@@ -9,7 +9,7 @@ class RouteController {
   listen() {
     this.app.get('/', (req, res) => {
       fs.readFile(this.path.join(__dirname, 'pages/index.html'), 'utf-8', (err, data) => {
-        const modifiedHTML = data.replaceAll('{{ valuePlaceholder }}', '12');
+        const modifiedHTML = data.replaceAll('{{ valuePlaceholder }}', '24');
 
         const matches = data.match(/\{\{.*?\}\}/g);
         console.log(matches.forEach(match => console.log(match.substring(2, match.length - 2))));
